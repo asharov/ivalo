1e8ad3c6bb591ed842e6e0cc0b7e87a812187090
Jaakko Kangasharju <ashar@iki.fi>
2014-07-04 01:24:50 +0200

    Initial Commit

---
+++ Ivalo/Ivalo.h
@@ -0,0 +1,19 @@
+//
+//  Ivalo.h
+//  Ivalo
+//
+//  Created by Jaakko Kangasharju on 04/07/14.
+//  Copyright (c) 2014 Jaakko Kangasharju. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Ivalo.
+FOUNDATION_EXPORT double IvaloVersionNumber;
+
+//! Project version string for Ivalo.
+FOUNDATION_EXPORT const unsigned char IvaloVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Ivalo/PublicHeader.h>
+
+
