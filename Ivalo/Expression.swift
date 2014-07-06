77c5d2169022b256da509506446f4aeaa4603621
Jaakko Kangasharju <ashar@iki.fi>
2014-07-06 21:30:00 +0200

    Make nonsensical constraints compile-time errors
    Create a marker protocol and implementations of it that are used as
    type parameters to equatable expressions. This way inappropriate
    constraints become compile-time errors because the type parameters
    do not match.

--- Ivalo/Expression.swift
+++ Ivalo/Expression.swift
@@ -14,25 +14,44 @@ operator prefix - {}
 operator postfix - {}
 operator infix ~=~ { associativity left precedence 130 }
 
-class ExpressionValue {
-    let expression: Expression
-    init (expression: Expression) {
+// A marker protocol to use as a type parameter to make nonsensical constraints,
+// like equating a left edge with a height, compile-time errors instead of run-time
+// assertion failures
+protocol ExpressionType {
+
+}
+
+struct HorizontalExpression : ExpressionType {
+
+}
+
+struct VerticalExpression : ExpressionType {
+
+}
+
+struct DimensionExpression : ExpressionType {
+
+}
+
+class ExpressionValue<T: ExpressionType> {
+    let expression: Expression<T>
+    init (expression: Expression<T>) {
         self.expression = expression
     }
 }
 
-enum Expression {
+enum Expression<T: ExpressionType> {
     case Number(CGFloat)
     case ViewEdge(Edge, UIView)
     case ViewDimension(Dimension, UIView)
-    case LinearFunction(CGFloat, ExpressionValue, CGFloat)
+    case LinearFunction(CGFloat, ExpressionValue<T>, CGFloat)
 }
 
-struct Equality {
-    let expressions: Expression[]
+struct Equality<T: ExpressionType> {
+    let expressions: Expression<T>[]
 }
 
-func expressionToLayout (expression: Expression) -> (view: UIView?, attribute: NSLayoutAttribute, multiplier: CGFloat, constant: CGFloat) {
+func expressionToLayout<T: ExpressionType> (expression: Expression<T>) -> (view: UIView?, attribute: NSLayoutAttribute, multiplier: CGFloat, constant: CGFloat) {
     switch (expression) {
     case .Number(let value):
         return (nil, .NotAnAttribute, 0.0, value)
@@ -46,17 +65,17 @@ func expressionToLayout (expression: Expression) -> (view: UIView?, attribute: N
     }
 }
 
-func equalityConstraint (left: Expression, right: Expression) -> NSLayoutConstraint {
+func equalityConstraint<T: ExpressionType> (left: Expression<T>, right: Expression<T>) -> NSLayoutConstraint {
     let leftLayout = expressionToLayout(left)
     let rightLayout = expressionToLayout(right)
     return NSLayoutConstraint(item: leftLayout.view, attribute: leftLayout.attribute, relatedBy: NSLayoutRelation.Equal, toItem: rightLayout.view, attribute: rightLayout.attribute, multiplier: rightLayout.multiplier / leftLayout.multiplier, constant: (rightLayout.constant - leftLayout.constant) / leftLayout.multiplier)
 }
 
-@prefix func | (view: UIView) -> Expression {
+@prefix func | (view: UIView) -> Expression<HorizontalExpression> {
     return .ViewEdge(.Left, view)
 }
 
-@prefix func | (expression: Expression) -> Expression {
+@prefix func | (expression: Expression<HorizontalExpression>) -> Expression<DimensionExpression> {
     switch expression {
     case .ViewEdge(.Right, let view):
         return .ViewDimension(.Width, view)
@@ -66,15 +85,15 @@ func equalityConstraint (left: Expression, right: Expression) -> NSLayoutConstra
     }
 }
 
-@postfix func | (view: UIView) -> Expression {
+@postfix func | (view: UIView) -> Expression<HorizontalExpression> {
     return .ViewEdge(.Right, view)
 }
 
-@prefix func - (view: UIView) -> Expression {
+@prefix func - (view: UIView) -> Expression<VerticalExpression> {
     return .ViewEdge(.Top, view)
 }
 
-@prefix func - (expression: Expression) -> Expression {
+@prefix func - (expression: Expression<VerticalExpression>) -> Expression<DimensionExpression> {
     switch expression {
     case .ViewEdge(.Bottom, let view):
         return .ViewDimension(.Height, view)
@@ -84,35 +103,35 @@ func equalityConstraint (left: Expression, right: Expression) -> NSLayoutConstra
     }
 }
 
-@postfix func - (view: UIView) -> Expression {
+@postfix func - (view: UIView) -> Expression<VerticalExpression> {
     return .ViewEdge(.Bottom, view)
 }
 
-func * (scalar: CGFloat, expression: Expression) -> Expression {
+func *<T: ExpressionType> (scalar: CGFloat, expression: Expression<T>) -> Expression<T> {
     return .LinearFunction(scalar, ExpressionValue(expression: expression), 0.0)
 }
 
-func + (expression: Expression, constant: CGFloat) -> Expression {
+func +<T: ExpressionType> (expression: Expression<T>, constant: CGFloat) -> Expression<T> {
     return .LinearFunction(1.0, ExpressionValue(expression: expression), constant)
 }
 
-func ~=~ (left: Expression, right: CGFloat) -> Equality {
-    return left ~=~ Expression.Number(right)
+func ~=~<T: ExpressionType> (left: Expression<T>, right: CGFloat) -> Equality<T> {
+    return left ~=~ .Number(right)
 }
 
-func ~=~ (left: Expression, right: Expression) -> Equality {
+func ~=~<T: ExpressionType> (left: Expression<T>, right: Expression<T>) -> Equality<T> {
     return Equality(expressions: [left, right])
 }
 
-func ~=~ (left: Equality, right: CGFloat) -> Equality {
-    return left ~=~ Expression.Number(right)
+func ~=~<T: ExpressionType> (left: Equality<T>, right: CGFloat) -> Equality<T> {
+    return left ~=~ .Number(right)
 }
 
-func ~=~ (left: Equality, right: Expression) -> Equality {
+func ~=~<T: ExpressionType> (left: Equality<T>, right: Expression<T>) -> Equality<T> {
     return Equality(expressions: left.expressions + [right])
 }
 
-func <-- (view: UIView, equality: Equality) -> UIView {
+func <--<T: ExpressionType> (view: UIView, equality: Equality<T>) -> UIView {
     for i in 1..equality.expressions.count {
         view <-- equalityConstraint(equality.expressions[i-1], equality.expressions[i])
     }
